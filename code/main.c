7360f86d1126264be61c41a9ed69f3d88db9b182
Sébastien Serre <sebastien.serre@sysmo.io>
2017-10-31 13:45:06 +0100

    tests

--- code/main.c
+++ code/main.c
@@ -3,6 +3,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+struct s {
+    int hello;
+};
+
+struct field {
+    struct s **set;
+    int nset;
+};
+
+void testarray() {
+    int a[10];
+    int *pa;
+    pa = &a;
+    int i;
+    for (i=0; i<10; i++) {
+        a[i] = i;
+    }
+    for (i=0; i<10; i++) {
+        printf("hello %i\n", pa[i]);
+    }
+}
+
+void testloop() {
+    struct s s1;
+    struct s s2;
+    struct s s3;
+    struct s s4;
+    s1.hello = 1;
+    s2.hello = 2;
+    s3.hello = 3;
+    s4.hello = 4;
+    int numset = 4, s;
+    struct s *g;
+
+    struct field f;
+    f.set = malloc(4 * sizeof(&s1));
+    f.set[0] = &s1;
+    f.set[1] = &s2;
+    f.set[2] = &s3;
+    f.set[3] = &s4;
+    f.nset = 4;
+
+    int i = 4;
+    struct s *l;
+    for(; i && (f.set++); i--) {
+        printf("arghhhh %i\n", f.set[0]->hello);
+    }
+}
+
 /**
  * @brief This is where we store our command line arguments
  */
@@ -70,6 +119,11 @@ main(int argc, char** argv) {
     if (GLOBAL_ARGS.dumpconfig > 0) {
         printf("Dump config \n");
         exit(EXIT_SUCCESS);
+    }
+
+
+    if (4 < 5) {
+        printf("hhhhhhhhhokkkkkkkkkk\n");
     }
 
     return EXIT_SUCCESS;
